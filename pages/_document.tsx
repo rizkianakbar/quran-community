927280cf9ccae2e1ae3bc86015f4aca2a9e6910b
rizkianakbar <rizkianakbar4@gmail.com>
2022-01-22 06:17:37 +0700

    chore(pwa): improve pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="id">
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
+          <meta charSet="UTF-8" />
+          <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
