1f050b31c3ee8e22a4c80b03681805d558ef3f8c
rizkianakbar <rizkianakbar4@gmail.com>
2022-01-22 06:00:56 +0700

    chore(pwa): setup web progressive apps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = nextConfig
+const settings = {
+  env: {},
+  devIndicators: {
+    autoPrerender: false,
+  },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+};
+
+module.exports =
+  process.env.NODE_ENV === 'development' ? settings : withPWA(settings);
