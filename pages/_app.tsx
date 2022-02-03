6167efe9b22746820d09f2fb3d52b1b7d581a9fa
rizkianakbar <rizkianakbar4@gmail.com>
2022-02-03 13:32:39 +0700

    feat: add top progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,38 @@
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
+
 import type { AppProps } from 'next/app';
 import { LayoutRoot } from '../components/layout/layout-root';
 import Head from 'next/head';
+import nProgress from 'nprogress';
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export const progress = nProgress.configure({
+  showSpinner: false,
+});
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    const handleStart = (_: string, { shallow }: { shallow: boolean }) => {
+      if (!shallow) {
+        progress.start();
+      }
+    };
+    const handleStop = () => {
+      progress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <LayoutRoot>
       <Head>
