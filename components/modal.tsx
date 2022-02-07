c9df6756227f98a1dad5acaea446c46bb4ec4972
rizkianakbar <rizkianakbar4@gmail.com>
2022-02-07 20:53:07 +0700

    fix: bg on dialog.overlay

--- components/modal.tsx
+++ components/modal.tsx
@@ -22,7 +22,7 @@ export default function Modal({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0" />
+              <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
             </Transition.Child>
 
             {/* This element is to trick the browser into centering the modal contents. */}
