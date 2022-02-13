d7a8dbf05e9055e113a27df928b5daf9a79b84e0
rizkianakbar <rizkianakbar4@gmail.com>
2022-02-13 18:07:48 +0700

    fix: change style the tabchild

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -14,11 +14,21 @@ const tabClasses = (isActive?: boolean) => {
 
 const tabChildClasses = (isActive?: boolean) => {
   return [
-    'text-sm font-bold w-full text-gray-400 p-3',
+    // give line right after the
+    //     article:after {
+    //     content: '';
+    //     position: absolute;
+    //     width: 100%;
+    //     height: 1px; // suit your need
+    //     background: black; // suit your need
+    //     top: 100%;
+    //     left: 0;
+    // }
+    'text-sm font-bold w-[30px] h-[30px] rounded-full mr-10 last:mr-0 relative after:article:after after:content[""] after:absolute after:w-[43px] after:h-[2px] after:bg-teal-500 after:top-1/2 after:left-full last:after:content[""] last:after:absolute last:after:w-full last:after:h-[0px] last:after:bg-white last:after:top-1/2 last:after:left-full',
     isActive
-      ? 'border-b-2 border-teal-500 text-teal-400'
-      : 'border-b-2 border-white',
-    'hover:border-b-2 hover:border-teal-500 hover:text-teal-400',
+      ? 'border-2 border-teal-500 text-white bg-teal-400'
+      : 'border-2 border-teal-500 text-teal-400',
+    'hover:border-2 hover:border-teal-500',
   ];
 };
 
@@ -109,8 +119,17 @@ export default function Tabs({ ayat, data }: any) {
               Menghafal Surah {data.name_latin} : {firstAyat}-{secondAyat}
             </p>
           </div>
-          <div className="flex justify-center bg-white">{renderChildTab}</div>
-          {isSelectedChild === '#1' && <div>Satu</div>}
+          <div className="flex justify-center bg-white p-2">
+            {renderChildTab}
+          </div>
+          {isSelectedChild === '#1' && (
+            <div className="flex justify-center bg-teal-100/40 p-2">
+              <p className="text-sm text-teal-500 text-center">
+                Hafalkan dengan teliti target hafalan arabic dan terjemahannya,
+                ulangi muratal sebanyak-banyaknya sampai hafal
+              </p>
+            </div>
+          )}
           {isSelectedChild === '#2' && <div>Dua</div>}
           {isSelectedChild === '#3' && <div>Tiga</div>}
         </>
