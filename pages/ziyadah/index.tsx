d74d3ca57c3fa36170dd9d864fc7b982cae347e9
rizkianakbar <rizkianakbar4@gmail.com>
2022-02-17 20:42:26 +0700

    fix: remove the breadcrumbs on ziyadah page

--- pages/ziyadah/index.tsx
+++ pages/ziyadah/index.tsx
@@ -1,31 +1,13 @@
-import { PageContent, PageSection } from '@/components/layout/pages';
-import { PageHeader } from '@/components/layout/pages/page-header';
+import { PageContent } from '@/components/layout/pages';
 import { QuranSelect } from '@/components/quran/quran-select';
-import { ButtonBack } from '@/components/ui/button-back';
 import { surahList } from '../../lib/surah/surah-list';
 
 export default function Ziyadah() {
-  const breadcrumbs = [
-    {
-      name: 'Ziyadah',
-      href: '/ziyadah',
-      current: true,
-    },
-  ];
-  const description =
-    'Tentukan surah dan ayat yang Anda ingin hafal, di sarankan target hafalan jangan terlalu panjang, perkirakan sesuai kemampuan hafalan Anda.';
-
   return (
-    <PageSection>
-      <PageHeader
-        backButton={<ButtonBack href="/" />}
-        breadcrumbs={breadcrumbs}
-        description={description}
-        title="Ziyadah"
-      />
+    <section className="flex flex-col flex-1 py-4">
       <PageContent>
         <QuranSelect surahList={surahList} />
       </PageContent>
-    </PageSection>
+    </section>
   );
 }
