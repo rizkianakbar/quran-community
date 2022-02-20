92e6a52cc47cbf9534195559b96bee839f6fb395
rizkianakbar <rizkianakbar4@gmail.com>
2022-02-21 06:21:33 +0700

    fix: change text color

--- components/quran/quran-header.tsx
+++ components/quran/quran-header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export function QuranHeader({ title }: any) {
   return (
     <>
-      <div className="bg-quranmemo-soft p-5 text-center text-quranmemo-primary rounded-lg shadow font-bold">
+      <div className="bg-quranmemo-soft p-5 text-center text-quranmemo-dark rounded-lg shadow font-bold">
         <h1 className="mb-5">{title}</h1>
         {/* when hover bounce */}
         <button className="mb-5 bg-quranmemo-primary px-3 py-1 text-white rounded mx-5">
