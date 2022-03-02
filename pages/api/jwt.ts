9053d3683781a6afb1fb7ddcc4373f07ce72127a
rizkianakbar <rizkianakbar4@gmail.com>
2022-03-02 12:22:44 +0700

    chore: setup prisma and next-auth

---
+++ pages/api/jwt.ts
@@ -0,0 +1,9 @@
+import { getToken } from 'next-auth/jwt';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const secret = process.env.SECRET;
+
+export default async function jwt(req: NextApiRequest, res: NextApiResponse) {
+  const token = await getToken({ req, secret });
+  res.send(JSON.stringify(token, null, 2));
+}
