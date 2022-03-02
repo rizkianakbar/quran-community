c9e9382e53aeba776ab6571cb123fc8f885258c9
rizkianakbar <rizkianakbar4@gmail.com>
2022-03-02 23:11:08 +0700

    fix: connect with database

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
+const prisma = new PrismaClient();
+
 export default NextAuth({
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
