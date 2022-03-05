fdab15c1c881bd2a8acb2c1772fc239f5171b098
rizkianakbar <rizkianakbar4@gmail.com>
2022-03-05 18:35:24 +0700

    feat: show list of hafalan from database

--- components/hafalan.tsx
+++ components/hafalan.tsx
@@ -1,8 +1,9 @@
+import { fetcher } from '@/utils/fetcher';
 import { BookOpenIcon } from '@heroicons/react/solid';
 import clsx from 'clsx';
 import React from 'react';
 import { Button } from './ui/button/button';
-
+import { surahList } from '../lib/surah/surah-list';
 const tabClasses = (isActive?: boolean) => {
   return [
     'text-sm font-bold w-full text-gray-400 p-3',
@@ -15,6 +16,23 @@ const tabClasses = (isActive?: boolean) => {
 
 export function Hafalan({ callback }: { callback: any }) {
   const [isSelectedParent, setIsSelectedParent] = React.useState('Ziyadah');
+  const [data, setData] = React.useState<any>([]);
+  const [hafalan, setHafalan] = React.useState([]);
+  React.useEffect(() => {
+    const getDataHafalan = async () => {
+      await fetcher('/api/get-hafalan').then((data: any) => {
+        setData(data);
+      });
+    };
+    getDataHafalan();
+  }, []);
+
+  data.forEach((item: any, index: number) => {
+    const newData = surahList.find((surah: any) => surah.id === item.id);
+    const assignNewData: any = Object.assign(newData, item);
+    data[index] = assignNewData;
+  });
+
   const [parenTab] = React.useState([
     {
       id: 1,
@@ -49,16 +67,46 @@ export function Hafalan({ callback }: { callback: any }) {
       </button>
     );
   });
+  // console.log(data);
+  // console.log(surahList);
+
   return (
     <div className="w-full mx-auto">
-      <div className="flex justify-center">{renderParentTab}</div>
-      <div className="flex justify-center mt-3">
-        <p>Belum ada data!</p>
+      <div className="flex justify-center fixed w-full bg-white">
+        {renderParentTab}
       </div>
-
-      {/* make a div that stick to the bottom of the screen*/}
+      {isSelectedParent === 'Ziyadah' && (
+        <>
+          {data.length > 0 ? (
+            <div className="text-gray-500 divide-y divide-gray-300 divide-dashed pt-10">
+              {data.map((item: any) => {
+                return (
+                  <div key={item.id} className="flex justify-center py-6 mx-4">
+                    <span className="text-sm">
+                      {item.name} : {item.startAyatId}-{item.endAyatId}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div className="flex justify-center mt-3 text-gray-500">
+              <p>Belum ada data!</p>
+            </div>
+          )}
+        </>
+      )}
+      {isSelectedParent === 'Dikoreksi' && (
+        <div className="flex justify-center mt-3 text-gray-500 ">
+          <p>Belum ada data!</p>
+        </div>
+      )}
+      {isSelectedParent === 'Sudah Hafal' && (
+        <div className="flex justify-center mt-3 text-gray-500">
+          <p>Belum ada data!</p>
+        </div>
+      )}
       <div className="fixed bottom-0 w-full h-16">
-        {/* make a button in the right position */}
         <div className="flex justify-center">
           <Button
             secondary
