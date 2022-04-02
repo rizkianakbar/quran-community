7cb8aea049db2bfa36591105ec2500e042b7c72b
rizkianakbar <rizkianakbar4@gmail.com>
2022-04-03 06:19:32 +0700

    feat(hafalan): added the loading text

--- components/hafalan.tsx
+++ components/hafalan.tsx
@@ -83,76 +83,83 @@ export function Hafalan({ callback }: { callback: any }) {
       <div className="flex justify-center fixed w-full bg-white">
         {renderParentTab}
       </div>
-      {isSelectedParent === 'Ziyadah' && (
+      {belumHafal.length === 0 && sudahHafal.length === 0 ? (
+        <p className="pt-14 text-center"> loading...</p>
+      ) : (
         <>
-          {belumHafal.length > 0 ? (
-            <div className="text-gray-500 divide-y divide-gray-300 divide-dashed pt-12">
-              {belumHafal.map((item: any) => {
-                return (
-                  <>
-                    <div key={item.id} className="flex py-6 mx-4">
-                      <span className="mr-auto text-sm ml-5">2</span>
-                      <span className="text-sm text-center m-auto">
-                        {item.name} : {item.startAyatId}-{item.endAyatId}
-                      </span>
-                      <button
-                        onClick={async () => {
-                          const body = {
-                            id: item.id,
-                            status: 1,
-                          };
-                          await fetcher('/api/set-sudah-hafal', {
-                            user: body,
-                          });
-                          setReload(reload + 1);
-                        }}
-                        className="text-sm ml-auto border-2 border-quranmemo-primary cursor-pointer rounded-full w-5 h-5 mr-5 hover:bg-quranmemo-primary"
-                      ></button>
-                    </div>
-                    <div>
-                      <div className="flex text-center text-gray-400 text-sm bg-gray-100 p-2 text-xs">
-                        <span className="mr-auto ml-8 cursor-pointer">
-                          {React.createElement(ExternalLinkIcon, {
-                            className: 'w-5 h-5 inline-block mr-1 mb-1',
-                          })}
-                          EDIT
-                        </span>
-                        <button
-                          onClick={async () => {
-                            const body = {
-                              id: item.id,
-                            };
-                            await fetcher('/api/delete-hafalan', {
-                              user: body,
-                            });
-                            setReload(reload + 1);
-                          }}
-                          className="m-auto cursor-pointer"
-                        >
-                          {React.createElement(TrashIcon, {
-                            className: 'w-5 h-5 inline-block mr-1 mb-1',
-                          })}
-                          HAPUS
-                        </button>
-                        <span className="ml-auto mr-8 cursor-pointer">
-                          {React.createElement(StarIcon, {
-                            className: 'w-5 h-5 inline-block mr-1 mb-1',
-                          })}
-                          IN PROGRESS
-                        </span>
-                      </div>
-                    </div>
-                  </>
-                );
-              })}
-            </div>
-          ) : (
-            <div className="flex justify-center text-gray-500 pt-14">
-              <p>Belum ada data!</p>
-            </div>
+          {isSelectedParent === 'Ziyadah' && (
+            <>
+              {belumHafal.length > 0 ? (
+                <div className="text-gray-500 divide-y divide-gray-300 divide-dashed pt-12">
+                  {belumHafal.map((item: any) => {
+                    return (
+                      <>
+                        <div key={item.id} className="flex py-6 mx-4">
+                          <span className="mr-auto text-sm ml-5">2</span>
+                          <span className="text-sm text-center m-auto">
+                            {item.name} : {item.startAyatId}-{item.endAyatId}
+                          </span>
+                          <button
+                            onClick={async () => {
+                              const body = {
+                                id: item.id,
+                                status: 1,
+                              };
+                              await fetcher('/api/set-sudah-hafal', {
+                                user: body,
+                              });
+                              setReload(reload + 1);
+                            }}
+                            className="text-sm ml-auto border-2 border-quranmemo-primary cursor-pointer rounded-full w-5 h-5 mr-5 hover:bg-quranmemo-primary"
+                          ></button>
+                        </div>
+                        <div>
+                          <div className="flex text-center text-gray-400 text-sm bg-gray-100 p-2 text-xs">
+                            <span className="mr-auto ml-8 cursor-pointer">
+                              {React.createElement(ExternalLinkIcon, {
+                                className: 'w-5 h-5 inline-block mr-1 mb-1',
+                              })}
+                              EDIT
+                            </span>
+                            <button
+                              onClick={async () => {
+                                const body = {
+                                  id: item.id,
+                                };
+                                await fetcher('/api/delete-hafalan', {
+                                  user: body,
+                                });
+                                setReload(reload + 1);
+                              }}
+                              className="m-auto cursor-pointer"
+                            >
+                              {React.createElement(TrashIcon, {
+                                className: 'w-5 h-5 inline-block mr-1 mb-1',
+                              })}
+                              HAPUS
+                            </button>
+                            <span className="ml-auto mr-8 cursor-pointer">
+                              {React.createElement(StarIcon, {
+                                className: 'w-5 h-5 inline-block mr-1 mb-1',
+                              })}
+                              IN PROGRESS
+                            </span>
+                          </div>
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div className="flex justify-center text-gray-500 pt-14">
+                  <p>Belum ada data!</p>
+                </div>
+              )}
+            </>
           )}
         </>
       )}
+
       {isSelectedParent === 'Dikoreksi' && (
         <div className="flex justify-center text-gray-500 pt-14">
           <p>Belum ada data!</p>
