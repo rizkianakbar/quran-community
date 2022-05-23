0440c8eee80fd3d5f92ff70e9e199600794e630f
Rizkian Akbar Juniansyah <rizkianakbar4@gmail.com>
2022-05-23 20:44:37 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,39 +9,8 @@ import { fetcher } from '@/utils/fetcher';
 import React from 'react';
 
 const Home: NextPage = () => {
-  React.useEffect(() => {
-    const getDataHafalan = async () => {
-      await fetcher('/api/get-comment').then((data: any) => {
-        console.log(data);
-      });
-    };
-    getDataHafalan();
-  }, []);
   return (
     <PageSection>
-      <Button
-        secondary
-        onClick={() => {
-          const data = {
-            name: 'ini terbaru',
-            comment: 'asdasdas',
-          };
-
-          fetcher('/api/set-comment', { user: data });
-        }}
-      >
-        comment
-      </Button>
-
-      <Button
-        secondary
-        onClick={() => {
-          const data = fetcher('/api/get-comment');
-          console.log(data);
-        }}
-      >
-        Show comment
-      </Button>
       <HomeHeader />
       <HomeContent>
         <Container className="space-y-3">
