7201958f0393665fccdcc0bc2e526800c919cfac
rizkianakbar <rizkianakbar4@gmail.com>
2022-06-10 09:03:45 +0700

    chore: add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,19 @@ class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
           <meta content="#5EEAD3" name="theme-color" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <meta content="QuranMemo" name="og:title" />
+          <meta
+            content="Menghafal Al-Quran di era digital"
+            name="og:description"
+          />
+          <meta content="https://quranmemo.vercel.app/" name="og:url" />
+          <meta content="QuranMemo" name="og:site_name" />
+          <meta content="website" name="og:type" />
+          <meta
+            content="https://res.cloudinary.com/rizkianakbar/image/upload/v1654826586/icon-256x256_myje8c.png"
+            name="og:image"
+          />
         </Head>
         <body>
           <Main />
